051f9ecbecbb94a36eb46db5e6b9e9b674a2b712
Semen <semen.manushko@gmail.com>
2022-08-02 10:45:52 +0200

    changed variables

--- support_files/resources.py
+++ support_files/resources.py
@@ -4,5 +4,4 @@ import datetime
 current_year = datetime.datetime.now().year
 
 weather_items = ['daily-precipitation', 'max-temperature', 'min-temperature', 'average-temperature']
-all_weather_items_pretty = ['Daily Precipitation', 'Max Temperature', 'Min Temperature', 'Average Temperature',
-                            'Snowfall', 'Leaf Area Index Low Vegetation Mean', 'Leaf Area Index Low Vegetation Max', 'Leaf Area Index Low Vegetation Min']
+all_weather_items_pretty = ['Daily Precipitation', 'Max Temperature', 'Min Temperature', 'Average Temperature']
