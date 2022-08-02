9edf17fedd96e872ee8803485bd6a2a45fa224cb
Semen <semen.manushko@gmail.com>
2022-08-02 11:13:42 +0200

    changed era5

--- support_files/resources.py
+++ support_files/resources.py
@@ -9,7 +9,8 @@ weather_items = ['daily-precipitation', 'max-temperature', 'min-temperature', 'a
 all_weather_items_pretty = ['Daily Precipitation', 'Max Temperature', 'Min Temperature', 'Average Temperature']
 
 
-mypath = './data_era5/'
+# mypath = './data_era5/'
+mypath = r"C:\Users\s.manushko\production\australia\data_era5/"
 all_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 start_date = (datetime.datetime.strptime(all_files[-1].split('_')[0], '%Y-%m-%d')+ datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 today = pd.to_datetime("today").strftime('%Y-%m-%d')
